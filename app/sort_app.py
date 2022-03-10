a3ff574322f783e6bdf8101699ddc36a4b44a8b0
jacob8707 <100741763+jacob8707@users.noreply.github.com>
2022-03-10 14:36:48 -0500

    Update sort_app.py

--- app/sort_app.py
+++ app/sort_app.py
@@ -1,5 +1,5 @@
 from SRC.sort_list  import sort_value
-Country_list=list(('Canada', 'Japan', 'Germany', 'Switzerland', 'Australia', 'United States of America', 'France', 'United Kingdom'))
+Country_list=list(('Canada', 'Japan', 'Germany', 'Switzerland', 'Australia', 'United States of America', 'France', 'Britain'))
 per_GDP_list=list((46327, 40113, 46468, 85300, 55057, 65280, 40380,42540))
 print('the sorted GDP is', sort_value(dict(zip(Country_list,per_GDP_list))))
 

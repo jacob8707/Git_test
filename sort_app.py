9f3b071a019898679eb92666255c9467cd81829c
Jacob8707 <shisailinghang@gmail.com>
2022-03-09 23:44:08 -0500

    change added

--- sort_app.py
+++
@@ -1,5 +0,0 @@
-from SRC.sort_list  import sort_value
-Country_list=list(('Canada', 'Japan', 'Germany', 'Switzerland', 'Australia', 'USA', 'France', 'UK'))
-per_GDP_list=list((46327, 40113, 46468, 85300, 55057, 65280, 40380,42540))
-print(sort_value(dict(zip(Country_list,per_GDP_list))))
-
